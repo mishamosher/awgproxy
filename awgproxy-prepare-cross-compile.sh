17ddfc164e17c8a00f5597ed93e217694b9fba3f
Elmer Miroslav Mosher Golovin <miroslav@mishamosher.com>
2025-07-20 01:07:02 +0300

    Update Android NDK.

--- awgproxy-prepare-cross-compile.sh
+++ awgproxy-prepare-cross-compile.sh
@@ -2,6 +2,6 @@
 
 go mod tidy -v
 mkdir android_ndk
-wget --no-verbose https://dl.google.com/android/repository/android-ndk-r27c-linux.zip -O android_ndk/ndk.zip
+wget --no-verbose https://dl.google.com/android/repository/android-ndk-r27d-linux.zip -O android_ndk/ndk.zip
 unzip android_ndk/ndk.zip -d android_ndk
 mv "android_ndk/$(ls -1 -d android_ndk/*/ | cut -f 2 -d '/' | head -1)" android_ndk/extracted
